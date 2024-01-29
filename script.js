1cf50b384ae1aefd0651f505d56ec1fbbc8df29a
lolodev <horaciodev1706@gmail.com>
2024-01-28 23:44:02 -0300

    Solving issues with playGame()

--- script.js
+++ script.js
@@ -18,23 +18,43 @@ const getComputerChoice = () => {
     }
 }
 
-let playerSelection = prompt('Rock, Paper, or Scissors?');
-console.log(playerSelection);
+const getPlayerSelection = () => {
+    let playerChoice = prompt('Rock, Paper, or Scissors?');
+    playerChoice = playerChoice.toLowerCase();
+    return playerChoice;
+}
 
-playerSelection = playerSelection.toLowerCase();
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
+let playerPoints = 0;
+let computerPoints = 0;
+
+function playGame() {
+    if (playerPoints === 5) {
+        console.log('You won the game!');
+    } else if (computerPoints === 5) {
+        console.log('You lost the game!');
+    }
+
+    const playerSelection = getPlayerSelection();
+    console.log(playerSelection);
+
+    const computerSelection = getComputerChoice();
+    console.log(computerSelection);
+
+    playRound(playerSelection, computerSelection);
+}
 
 function playRound(playerSelection, computerSelection) {
     console.log(playerSelection, computerSelection);
     if (playerSelection === computerSelection) {
         return 'It\'s a tie!';
     } else if (playerSelection === 'rock' && computerSelection === 'scissors' || playerSelection === 'paper' && computerSelection === 'rock' || playerSelection === 'scissors' && computerSelection === 'paper') {
+        playerPoints++;
         return `You win! ${playerSelection} beats ${computerSelection}`;
     } else if (playerSelection === 'rock' && computerSelection === 'paper' || playerSelection === 'paper' && computerSelection === 'scissors' || playerSelection === 'scissors' && computerSelection === 'rock') {
+        computerPoints++;
         return `You lose! ${computerSelection} beats ${playerSelection}`;
     }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+playGame();
 
